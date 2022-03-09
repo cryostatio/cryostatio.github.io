cd245edd505fee987865ec2231ab48d2c02f0a65
Janelle Law <jalaw@redhat.com>
2022-03-09 13:46:12 -0500

    Preserve older get-started versions

--- assets/javascript/guides-version-dropdown.js
+++ assets/javascript/guides-version-dropdown.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
     $('#guidesindex-version-dropdown').on('change', function() {
-      var guideUrl = window.location.href.substring(window.location.href.indexOf("/guides/"));
-  
+      const guidePages = ["/guides/", "/get-started/"];
+      var guideUrl = window.location.href;
+
+      for(const gp of guidePages) {
+        const idx = window.location.href.indexOf(gp);
+        if(idx > -1) {
+          guideUrl = window.location.href.substring(idx);
+          break;
+        }
+      };
+
       if (this.value == 'latest') {
-        window.location.href = '/guides/';
+        window.location.href = guideUrl;
       } else {
         window.location.href = '/version/' + this.value + guideUrl;
       }
