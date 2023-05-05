ed6511eb32cf729c173ee0abcd46921237d93b92
Andrew Azores <aazores@redhat.com>
2023-05-05 13:49:14 -0400

    trim report json

--- demo/fake-api.js
+++ demo/fake-api.js
@@ -245,80 +245,77 @@ createServer({
         return new Response(200);
       }
     );
-    this.get(
-      "/api/beta/reports/:targetId/:recordingName",
-      () => {
-        return new Response(
-          200,
-          {},
-          {
-            Demo: {
-              score: 100,
-              name: "Fake Demo Result",
-              topic: "demo",
-              description:
-                "Remember, all of this data is static, fake, and entirely within your browser.",
-            },
-            VMOperations: {
-              score: 0.5943414499999999,
-              name: "VMOperation Peak Duration",
-              topic: "vm_operations",
-              description:
-                "Summary:\nNo excessively long VM operations were found in this recording (the longest was 23.774 ms).",
-            },
-            PasswordsInSystemProperties: {
-              score: 75.0,
-              name: "Passwords in System Properties",
-              topic: "system_properties",
-              description:
-                "Summary:\nThe system properties in the recording may contain passwords.\n\nExplanation:\nThe following suspicious system properties were found in this recording: javax.net.ssl.keyStorePassword,javax.net.ssl.trustStorePassword,com.sun.management.jmxremote.password.file. The following regular expression was used to exclude strings from this rule: \u0027\u0027(passworld|passwise)\u0027\u0027.\n\nSolution:\nIf you wish to keep having passwords in your system properties, but want to be able to share recordings without also sharing the passwords, please disable the \u0027\u0027Initial System Property\u0027\u0027 event.",
-            },
-            Options: {
-              score: 0.0,
-              name: "Command Line Options Check",
-              topic: "jvm_information",
-              description:
-                "Summary:\nNo undocumented, deprecated or non-recommended option flags were detected.",
-            },
-            PasswordsInEnvironment: {
-              score: 75.0,
-              name: "Passwords in Environment Variables",
-              topic: "environment_variables",
-              description:
-                "Summary:\nThe environment variables in the recording may contain passwords.\n\nExplanation:\nThe following suspicious environment variables were found in this recording: CRYOSTAT_JDBC_PASSWORD, CRYOSTAT_JMX_CREDENTIALS_DB_PASSWORD. The following regular expression was used to exclude strings from this rule: \u0027\u0027(passworld|passwise)\u0027\u0027.\n\nSolution:\nIf you wish to keep having passwords in your environment variables, but want to be able to share recordings without also sharing the passwords, please disable the \u0027\u0027Initial Environment Variable\u0027\u0027 event.",
-            },
-            MethodProfiling: {
-              score: 0.6705776661956153,
-              name: "Method Profiling",
-              topic: "method_profiling",
-              description:
-                "Summary:\nNo methods where optimization would be particularly efficient could be detected.",
-            },
-            ManyRunningProcesses: {
-              score: 0.20309488837692125,
-              name: "Competing Processes",
-              topic: "processes",
-              description:
-                "Summary:\n1 processes were running while this Flight Recording was made.\n\nExplanation:\nAt 5/5/23, 5:17:27.180 PM, a total of 1 other processes were running on the host machine that this Flight Recording was made on.\n\nSolution:\nIf this is a server environment, it may be good to only run other critical processes on that machine.",
-            },
-            StackdepthSetting: {
-              score: 25.0,
-              name: "Stackdepth Setting",
-              topic: "jvm_information",
-              description:
-                "Summary:\nSome stack traces were truncated in this recording.\n\nExplanation:\nThe Flight Recorder is configured with a maximum captured stack depth of 64. 3.11 % of all traces were larger than this option, and were therefore truncated. If more detailed traces are required, increase the \u0027\u0027-XX:FlightRecorderOptions\u003dstackdepth\u003d\u003cvalue\u003e\u0027\u0027 value.\nEvents of the following types have truncated stack traces: org.openjdk.jmc.flightrecorder.rules.jdk.general.StackDepthSettingRule$StackDepthTruncationData@21e159e2,org.openjdk.jmc.flightrecorder.rules.jdk.general.StackDepthSettingRule$StackDepthTruncationData@174930bc,org.openjdk.jmc.flightrecorder.rules.jdk.general.StackDepthSettingRule$StackDepthTruncationData@4f5d6223",
-            },
-            PasswordsInArguments: {
-              score: 0.0,
-              name: "Passwords in Java Arguments",
-              topic: "jvm_information",
-              description:
-                "Summary:\nThe recording does not seem to contain passwords in the application arguments.",
-            },
-          }
-        );
-      }
-    );
+    this.get("/api/beta/reports/:targetId/:recordingName", () => {
+      return new Response(
+        200,
+        {},
+        {
+          Demo: {
+            score: 100,
+            name: "Fake Demo Result",
+            topic: "demo",
+            description:
+              "Remember, all of this data is static, fake, and entirely within your browser.",
+          },
+          VMOperations: {
+            score: 0.5943414499999999,
+            name: "VMOperation Peak Duration",
+            topic: "vm_operations",
+            description:
+              "Summary:\nNo excessively long VM operations were found in this recording (the longest was 23.774 ms).",
+          },
+          PasswordsInSystemProperties: {
+            score: 75.0,
+            name: "Passwords in System Properties",
+            topic: "system_properties",
+            description:
+              "Summary:\nThe system properties in the recording may contain passwords.\n\nExplanation:\nThe following suspicious system properties were found in this recording: javax.net.ssl.keyStorePassword,javax.net.ssl.trustStorePassword,com.sun.management.jmxremote.password.file. The following regular expression was used to exclude strings from this rule: \u0027\u0027(passworld|passwise)\u0027\u0027.\n\nSolution:\nIf you wish to keep having passwords in your system properties, but want to be able to share recordings without also sharing the passwords, please disable the \u0027\u0027Initial System Property\u0027\u0027 event.",
+          },
+          Options: {
+            score: 0.0,
+            name: "Command Line Options Check",
+            topic: "jvm_information",
+            description:
+              "Summary:\nNo undocumented, deprecated or non-recommended option flags were detected.",
+          },
+          PasswordsInEnvironment: {
+            score: 75.0,
+            name: "Passwords in Environment Variables",
+            topic: "environment_variables",
+            description:
+              "Summary:\nThe environment variables in the recording may contain passwords.\n\nExplanation:\nThe following suspicious environment variables were found in this recording: CRYOSTAT_JDBC_PASSWORD, CRYOSTAT_JMX_CREDENTIALS_DB_PASSWORD. The following regular expression was used to exclude strings from this rule: \u0027\u0027(passworld|passwise)\u0027\u0027.\n\nSolution:\nIf you wish to keep having passwords in your environment variables, but want to be able to share recordings without also sharing the passwords, please disable the \u0027\u0027Initial Environment Variable\u0027\u0027 event.",
+          },
+          MethodProfiling: {
+            score: 0.6705776661956153,
+            name: "Method Profiling",
+            topic: "method_profiling",
+            description:
+              "Summary:\nNo methods where optimization would be particularly efficient could be detected.",
+          },
+          ManyRunningProcesses: {
+            score: 0.20309488837692125,
+            name: "Competing Processes",
+            topic: "processes",
+            description:
+              "Summary:\n1 processes were running while this Flight Recording was made.\n\nExplanation:\nAt 5/5/23, 5:17:27.180 PM, a total of 1 other processes were running on the host machine that this Flight Recording was made on.\n\nSolution:\nIf this is a server environment, it may be good to only run other critical processes on that machine.",
+          },
+          StackdepthSetting: {
+            score: 25.0,
+            name: "Stackdepth Setting",
+            topic: "jvm_information",
+            description:
+              "Summary:\nSome stack traces were truncated in this recording.\n\nExplanation:\nThe Flight Recorder is configured with a maximum captured stack depth of 64. 3.11 % of all traces were larger than this option, and were therefore truncated. If more detailed traces are required, increase the \u0027\u0027-XX:FlightRecorderOptions\u003dstackdepth\u003d\u003cvalue\u003e\u0027\u0027 value.\nEvents of the following types have truncated stack traces: org.openjdk.jmc.flightrecorder.rules.jdk.general.StackDepthSettingRule$StackDepthTruncationData@21e159e2,org.openjdk.jmc.flightrecorder.rules.jdk.general.StackDepthSettingRule$StackDepthTruncationData@174930bc,org.openjdk.jmc.flightrecorder.rules.jdk.general.StackDepthSettingRule$StackDepthTruncationData@4f5d6223",
+          },
+          PasswordsInArguments: {
+            score: 0.0,
+            name: "Passwords in Java Arguments",
+            topic: "jvm_information",
+            description:
+              "Summary:\nThe recording does not seem to contain passwords in the application arguments.",
+          },
+        }
+      );
+    });
     this.get("/api/v1/targets/:targetId/recordingOptions", () => []);
     this.get("/api/v1/targets/:targetId/events", () => [
       {
